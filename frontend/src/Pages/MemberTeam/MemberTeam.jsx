8873164d39e3d8df7df0b09611340c21b5bd3745
Fl@vio <ramanantsoa1311@gmail.com>
2022-11-06 16:01:59 +0300

    view of all the members of the team

--- frontend/src/Pages/MemberTeam/MemberTeam.jsx
+++ frontend/src/Pages/MemberTeam/MemberTeam.jsx
@@ -1,7 +1,48 @@
+import { CardMember } from '../../Components/Navbar/Card-member/CardMember';
+import './MemberTeam.css';
+import Miarintsoa from '../../images/Miarintsoa.jpg';
+import Dina from '../../images/Dina.jpg';
+import Anjara from '../../images/Anjara.jpeg';
+import Flavio from '../../images/Flavio2.jpg';
+
 export const MemberTeam = () => {
+    
+    const members = [
+        {
+            picture: Flavio,
+            nom: 'Flavio Ramanantsoa',
+            spec: 'Développeur Fullstack Javascript'
+        },
+        {
+            picture: Miarintsoa,
+            nom: 'Miarintsoa Itokiana',
+            spec: 'Développeur PHP back-end'
+        },
+        {
+            picture: Dina,
+            nom: 'Dina Rasamimanana',
+            spec: 'Scrum master'
+        },
+        {
+            picture: Anjara,
+            nom: 'Miarintsoa Itokiana',
+            spec: 'Product Owner'
+        }
+    ];
+
     return(
         <div className="member-team">
-            De tsy aiko !!
+            <div className="list-member">
+                <h1>Membres</h1>
+                
+                <div className="list">
+                    {
+                        members.map(
+                            (member, index) => <CardMember member={member} key={index} />
+                        )
+                    }
+                </div>
+            </div>
         </div>
     )
 }
